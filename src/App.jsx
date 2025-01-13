225b9a04b9387dcd3fb0f7051f0f02239e08c34f
Solomon Nwante <myphilothea@gmail.com>
2025-01-13 05:28:17 +0100

    Add app component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { TaskFlux } from "./components/TaskFlux";
+
+function App() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <TaskFlux />
+    </div>
+  );
+}
+
+export default App;
